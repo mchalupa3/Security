7d3166dab1326f95888841d77045c86a1f4eb3ad
mchalupa3 <mchalupa3@gmail.com>
2018-02-14 13:48:19 -0600

    Update

---
+++ SecurityProgram/SecurityProgram/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace SecurityProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTurnOnFirewall = new System.Windows.Forms.Button();
+            this.btnTurnOnDefender = new System.Windows.Forms.Button();
+            this.btnAutomateDefender = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTurnOnFirewall
+            // 
+            this.btnTurnOnFirewall.Location = new System.Drawing.Point(66, 12);
+            this.btnTurnOnFirewall.Name = "btnTurnOnFirewall";
+            this.btnTurnOnFirewall.Size = new System.Drawing.Size(155, 23);
+            this.btnTurnOnFirewall.TabIndex = 0;
+            this.btnTurnOnFirewall.Text = "Turn On Firewall";
+            this.btnTurnOnFirewall.UseVisualStyleBackColor = true;
+            this.btnTurnOnFirewall.Click += new System.EventHandler(this.btnTurnOnFirewall_Click);
+            // 
+            // btnTurnOnDefender
+            // 
+            this.btnTurnOnDefender.Location = new System.Drawing.Point(66, 41);
+            this.btnTurnOnDefender.Name = "btnTurnOnDefender";
+            this.btnTurnOnDefender.Size = new System.Drawing.Size(155, 23);
+            this.btnTurnOnDefender.TabIndex = 1;
+            this.btnTurnOnDefender.Text = "Turn On Defender";
+            this.btnTurnOnDefender.UseVisualStyleBackColor = true;
+            this.btnTurnOnDefender.Click += new System.EventHandler(this.btnTurnOnDefender_Click);
+            // 
+            // btnAutomateDefender
+            // 
+            this.btnAutomateDefender.Location = new System.Drawing.Point(66, 71);
+            this.btnAutomateDefender.Name = "btnAutomateDefender";
+            this.btnAutomateDefender.Size = new System.Drawing.Size(155, 23);
+            this.btnAutomateDefender.TabIndex = 2;
+            this.btnAutomateDefender.Text = "Automate Defender";
+            this.btnAutomateDefender.UseVisualStyleBackColor = true;
+            this.btnAutomateDefender.Click += new System.EventHandler(this.btnAutomateDefender_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnAutomateDefender);
+            this.Controls.Add(this.btnTurnOnDefender);
+            this.Controls.Add(this.btnTurnOnFirewall);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTurnOnFirewall;
+        private System.Windows.Forms.Button btnTurnOnDefender;
+        private System.Windows.Forms.Button btnAutomateDefender;
+    }
+}
+
---
+++ SecurityProgram/SecurityProgram/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SecurityProgram
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTurnOnFirewall_Click(object sender, EventArgs e)
+        {
+            string strCmdText;
+            strCmdText = "/C NetSh Advfirewall set allprofiles state on";
+            System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+        }
+
+        private void btnTurnOnDefender_Click(object sender, EventArgs e)
+        {
+            string strCmdText;
+            strCmdText = "/C sc start WinDefend";
+            System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+        }
+
+        private void btnAutomateDefender_Click(object sender, EventArgs e)
+        {
+            string strCmdText;
+            strCmdText = "/C sc config WinDefend start= auto";
+            System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+        }
+    }
+}
